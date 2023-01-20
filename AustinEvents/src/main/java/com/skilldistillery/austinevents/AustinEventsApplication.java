0fa8eefe4f81230497435aedc010ab8dcdc5e4dd
Carlos Lobera <carlosclobera@gmail.com>
2023-01-20 17:31:44 -0600

    add dependancies

---
+++ AustinEvents/src/main/java/com/skilldistillery/austinevents/AustinEventsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.austinevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AustinEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AustinEventsApplication.class, args);
+	}
+
+}
---
+++ AustinEvents/src/test/java/com/skilldistillery/austinevents/AustinEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.austinevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AustinEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
